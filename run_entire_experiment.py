b3e92a4e44663ed7b554ed02cb48af769dd1beea
Ricardo Corral C <ricardo@suggestic.com>
2015-11-03 13:31:23 -0600

    h fix

--- run_entire_experiment.py
+++ run_entire_experiment.py
@@ -3,20 +3,22 @@ import computeACCdroppingFamily
 
 MONGODB_URI = 'mongodb://rcorral:mongolabpass12483@ds043022.mongolab.com:43022/rccvsseqaccs'
 collection_name = 'dropedclassaccs'
-client = MongoClient(MONGODB_URI)
-db = client.get_default_database()
-collection = db[collection_name]
+
 
 
 def get_hs(cathfile='CATHFINAL.txt'):
     hs = set()
     for l in open(cathfile,'r'):
         ll = l.strip().split(',')
-        _h = [-1]
+        _h = ll[-1]
+        print _h
         hs.add(_h)
     return list(hs)
 
 def run_experiment(hslist=[]):
+    client = MongoClient(MONGODB_URI)
+    db = client.get_default_database()
+    collection = db[collection_name]
     for h in hslist:
         if not collection.find_one({'droppedfam':h}):
             computeACCdroppingFamily.main_exec(familyToDrop=familyToDrop)
